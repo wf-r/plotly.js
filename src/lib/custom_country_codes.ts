734ce036ad1680b7e309e929b088ec2d3f4f2449
Cameron DeCoster <cameron.decoster@gmail.com>
2026-06-22 12:28:59 -0600

    feat: Replace country-regex with country-iso-search

---
+++ src/lib/custom_country_codes.ts
@@ -0,0 +1,39 @@
+import type { CountryRecord } from 'country-iso-search';
+
+export const COUNTRIES_X: ReadonlyArray<CountryRecord> = [
+    {
+        iso3: 'XAC',
+        iso2: '',
+        m49: '',
+        name: 'Aksai Chin',
+        aliases: []
+    },
+    {
+        iso3: 'XAP',
+        iso2: '',
+        m49: '',
+        name: 'Arunachal Pradesh',
+        aliases: []
+    },
+    {
+        iso3: 'XBT',
+        iso2: '',
+        m49: '',
+        name: 'Bir Tawil',
+        aliases: []
+    },
+    {
+        iso3: 'XHT',
+        iso2: '',
+        m49: '',
+        name: 'Halaib Triangle',
+        aliases: []
+    },
+    {
+        iso3: 'XJK',
+        iso2: '',
+        m49: '',
+        name: 'Jammu and Kashmir',
+        aliases: []
+    }
+];
